7fff44f485c443d5dd85b7d068b03c0f29fdc8ec
ea1gwm <ea1gwm@gmail.com>
2026-05-08 13:39:16 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@
    Eso fuerza al SW a invalidar la cache vieja.
    ============================================================= */
 
-const CACHE_VERSION = 'cg1-v0.7.19';
+const CACHE_VERSION = 'cg1-v0.7.20';
 const PRECACHE_URLS = [
   './',
   './index.html',
